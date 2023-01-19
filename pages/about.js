2e24d91b0b847215fe92cb740e3a6d99bf42f793
Diego Granados <holyhypo@gmail.com>
2023-01-19 00:10:57 -0500

    Finalizada la navegación

---
+++ pages/about.js
@@ -0,0 +1,7 @@
+import PageLayout from "@/components/PageLayout"
+
+export default function About(){
+    return <PageLayout title = 'About'>
+        <h1>Página de About</h1>
+    </PageLayout>
+}
---
+++ pages/article/[id].js
@@ -0,0 +1,5 @@
+export default function Article (){
+return (
+    <h1>Esto es un artículo</h1>
+)
+}
